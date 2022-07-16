d91594f5b136bea9b2a105a5fe83d0c99a0a54a7
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-07-16 14:54:29 +0430

    base styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header>
+      <section className="flex items-center space-x-2 md:space-x-10">
+        <img
+          src="https://rb.gy/ulxxee"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+
+        <ul>
+          <li className="headerLink cursor-default font-semibold text-white hover:text-white">
+            Home
+          </li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
+      </section>
+      <section></section>
+    </header>
+  );
 };
 
 export default Header;
