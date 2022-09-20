f25f63269a1c30f9d8d1f76fdfacb5ece563964d
reza_talebi <reza.talebi0902@gmail.com>
2022-09-20 15:47:57 +0430

    s

--- next.config.js
+++ next.config.js
@@ -1,15 +1,27 @@
+// /** @type {import('next').NextConfig} */
+
+// const withPWA = require("next-pwa")({
+//   dest: "public",
+//   register: true,
+//   skipWaiting: true,
+// });
+
+// module.exports = withPWA({
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   images: {
+//     domains: ["rb.gy", "image.tmdb.org"],
+//   },
+// });
+
 /** @type {import('next').NextConfig} */
 
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-});
-
-module.exports = withPWA({
+const NextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ["rb.gy", "image.tmdb.org"],
   },
-});
+};
+
+module.exports = NextConfig;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <Meta />
+          <Meta title="netflix" />
         </Head>
         <body>
           <Main />
